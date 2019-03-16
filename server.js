0de2cdb18905f16d408d0855b87f1a330e3a614d
ciberciv <vcasasm@hotmail.es>
2019-03-16 19:48:10 +0100

    Add POST requests (signin and register). No database yet

--- server.js
+++ server.js
@@ -1,14 +1,62 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "ciberciv",
+      email: "ciberciv@ciber.com",
+      password: "secret",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "124",
+      name: "vicrebic",
+      email: "vicrebic@ciber.com",
+      password: "secrettoo",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
 
 app.get("/", (req, res) => {
-  res.send("Working");
+  res.send(database.users);
+})
+
+app.post("/signin", (req, res) => {
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+    res.json("success");
+  } else {
+    res.status(400).json("error logging");
+  }
+  res.json("signin");
+})
+
+app.post("/register", (req, res) => {
+  const {email, name, password} = req.body;
+
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+
+  res.json(database.users[database.users.length - 1])
 })
 
 app.listen(3000, () => {
   console.log("app init");
 })
+
 
 /*
 / -> res = this is working
